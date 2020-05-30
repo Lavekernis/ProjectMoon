a0e38a759b0e386a49b7428dfcdb4d13488c17f6
Przemek Bieniek <p.bieniek4@student.uw.edu.pl>
2020-05-30 16:14:49 +0200

    plik ze zmiennymi

---
+++ variables.py
@@ -0,0 +1,15 @@
+#stałe układu - masy w kg, promienie i odległosci w km, okresy w dniach
+
+au = 149597870.7
+R_sun = 695700
+M_sun = 1.9884*10**30
+R_earth = 6371
+M_earth = 5.97237*10**24
+d_earth = 149.6*10**6 #średnia odległość Ziemi od Słońca
+T_earth = 365.256
+R_moon = 1737.4
+M_moon = 7.342*10**22
+d_moon = 385000
+T_moon = 27.32
+G = 6.674*10**-20 #stała grawitacyjna w km^3/kg*s
+d_mars = 228*10**6 #średnia odległość Marsa od Słońca
